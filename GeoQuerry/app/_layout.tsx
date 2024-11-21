74f9ab77d282fbab7c0400a600d89aff45e750d8
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-11-21 19:05:40 +0100

    Update _layout.tsx

--- GeoQuerry/app/_layout.tsx
+++ GeoQuerry/app/_layout.tsx
@@ -1,23 +1,23 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-//import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import Login from './auth/login';
-import SignUp from './auth/signup';
-// import { Container } from './styles';
-const Stack = createNativeStackNavigator()
+
+import TabLayout from './(tabs)/_layout';
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium':require('../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
   });
 
   useEffect(() => {
@@ -29,13 +29,14 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-
+  
+  const Stack = createNativeStackNavigator()
   return (
-    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator >
-        <Stack.Screen name='auth/login' component={Login} />
-        <Stack.Screen name='auth/signup' component={SignUp}/>
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name='(tabs)' component={TabLayout}/>
     </Stack.Navigator>
-    </ThemeProvider>
+    
   );
 }
