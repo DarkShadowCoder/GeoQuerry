e7645765696524c49192b05586f4ed4b856670e5
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-11-23 14:47:19 +0100

    Update MapScreens.tsx

--- GeoQuerry/app/(tabs)/MapScreens.tsx
+++ GeoQuerry/app/(tabs)/MapScreens.tsx
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
+      <MapView style={styles.map} 
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation={true}
+      />
     </View>
   )
 }
 
 export default MapScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+
+  },
+  map:{
+    width: "100%",
+    height: "100%"
+  }
+})
