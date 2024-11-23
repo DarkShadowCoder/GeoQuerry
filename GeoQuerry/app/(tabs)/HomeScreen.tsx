c7c6c541abf7e7f5c9db4beea74be444dd22a55f
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-11-23 14:47:14 +0100

    Update HomeScreen.tsx

--- GeoQuerry/app/(tabs)/HomeScreen.tsx
+++ GeoQuerry/app/(tabs)/HomeScreen.tsx
@@ -2,40 +2,50 @@ import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import Header from '@/components/Header'
 import { Colors } from '@/constants/Colors'
-import Ionicons from '@expo/vector-icons/Ionicons'
 import ContentItems from '@/components/ContentItems'
 
+
 const Home = () => {
+  //const { user } = useUser()
   const cours = false
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.hero}>
+    <View>
+        <View style={styles.container}>
+          <Image source={require('@/assets/images/Image2.png')}
+            alt='Background'
+            style={{
+              zIndex: -50,
+              position: "absolute",
+              objectFit: "contain"
+            }}
+            />
+            <Header />
+            <View style={styles.hero}>
+            </View>
+            
+            <View style={styles.mainContainer}>
+              <Text style={styles.title}>
+                Programmes
+              </Text>
+              {cours && (
+                <ContentItems 
+                image={require('@/assets/images/react-logo.png')}
+                teacher={"Dr. MAKA MAKA EBENEZER"}
+                title = {"Audit et Securité des Systemes d'informations"}
+                date="14 November"
+                />
+              )||
+              <View style={styles.notContent}>
+                <Text style={{
+                  fontSize: 25,
+                  fontFamily: 'outfit',
+                  color: Colors.SECONDARY,
+                }}>Aucun cours Programmé</Text>
+              </View>
+              }
+            </View>
 
-      </View>
-      
-      <View style={styles.mainContainer}>
-        <Text style={styles.title}>
-          Programmes
-        </Text>
-        {cours && (
-          <ContentItems 
-          image={require('@/assets/images/react-logo.png')}
-          teacher={"Dr. MAKA MAKA EBENEZER"}
-          title = {"Audit et Securité des Systemes d'informations"}
-          date="14 November"
-          />
-        )||
-        <View style={styles.notContent}>
-          <Text style={{
-            fontSize: 25,
-            fontFamily: 'outfit',
-            color: Colors.light.secondary,
-          }}>Aucun cours Programmé</Text>
-        </View>
-        }
-      </View>
-
+          </View>
     </View>
   )
 }
@@ -51,7 +61,7 @@ const styles = StyleSheet.create({
     paddingVertical: 25,
     width: '100%',
     height: '100%',
-    backgroundColor: Colors.light.background
+    backgroundColor: Colors.WHITE
   },
   hero:{
 
