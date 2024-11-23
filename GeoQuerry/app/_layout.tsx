fffac1827c086d3a716fb05135f3aa0ddde7da8d
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-11-23 14:47:27 +0100

    Update _layout.tsx

--- GeoQuerry/app/_layout.tsx
+++ GeoQuerry/app/_layout.tsx
@@ -1,19 +1,64 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import * as SecureStore from 'expo-secure-store'
+import LoginScreen from './(tabs)/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './(tabs)/HomeScreen';
+import { MyTabs } from '@/components/navigation/BottomNavigation';
+import { NavigationContainer } from '@react-navigation/native';
+import MapScreen from './(tabs)/MapScreens';
+import * as Location from 'expo-location'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
-import TabLayout from './(tabs)/_layout';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+const tokenCache = {
+  async getToken(key){
+    try {
+      return SecureStore.getItemAsync(key);
+    }catch (err) {
+      return null;
+    }
+  },
+  async setToken(key, value){
+    try {
+      return SecureStore.setItemAsync(key, value);
+    }catch (err) {
+      return ;
+    }
+  }
+}
+
+const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location)
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
     'outfit-medium':require('../assets/fonts/Outfit-Medium.ttf'),
@@ -30,13 +75,32 @@ export default function RootLayout() {
     return null;
   }
   
-  const Stack = createNativeStackNavigator()
+  //const {user} = useUser()
   return (
-    <Stack.Navigator screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack.Screen name='(tabs)' component={TabLayout}/>
-    </Stack.Navigator>
+    /*<ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey={'pk_test_Y2FyaW5nLW1vbGUtNzEuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
+      <SignedIn>
+        <MyTabs />
+      </SignedIn>
+      <SignedOut>
+        <View style={styles.container}>
+          <LoginScreen />
+        </View>
+      </SignedOut>
+    </ClerkProvider>
     
+   <NavigationContainer>
+      <MyTabs />
+   </NavigationContainer>
+   */
+  <MapScreen />
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    width: '100%',
+    height: '100%',
+  }
+})
